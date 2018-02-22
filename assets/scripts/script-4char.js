cc646836c24340662586c1ed5aaad9e4489f24bf
Cender <CenderG@win.dowjones.net>
2018-02-21 23:35:19 -0500

    my message

--- assets/scripts/script-4char.js
+++ assets/scripts/script-4char.js
@@ -7,8 +7,8 @@ var game = {
   attackerId: "",
   defender: "",
   defenderId: "",
-  defenderCnt: 4,
-  killCnt: 0,
+  defenderCnt: 3,
+  killCnt: 1,
   wins: 0,
   losses: 0
 };
@@ -56,8 +56,6 @@ $(document).ready(function () {
         $(".enemy").filter(".C3").hide();
       } else if ($(this).hasClass("C4")) {
         $(".enemy").filter(".C4").hide();
-      }  else if ($(this).hasClass("C5")) {
-        $(".enemy").filter(".C5").hide();
       }
       game.attacker = this;
       game.attackerId = $(this).attr("id");
@@ -80,7 +78,7 @@ $(document).ready(function () {
     console.log(this);
     // $(this).siblings(".enemy").hide();
     if ($("#btn").text() !== "Attack") {
-      $("#msg").text("Press attack character or button to attack the defender");
+      $("#msg").text("Press button to attack the defender");
       //$("#msg").text("Choose your Enemy");
       $("#btn").show();
       $("#btn").text("Attack");
@@ -92,27 +90,22 @@ $(document).ready(function () {
         game.defenderId = $("#D1").attr("id");
         game.defender = document.getElementById("D1");
         $(".defender").filter("#D1").show();
-        $(".defender").filter("#D2, #D3, #D4, #D5").hide();
+        $(".defender").filter("#D2, #D3, #D4").hide();
       } else if ($(this).hasClass("C2")) {
         game.defender = document.getElementById("D2");
         game.defenderId = $("#D2").attr("id");
         $(".defender").filter("#D2").show();
-        $(".defender").filter("#D1, #D3, #D4, #D5").hide();
+        $(".defender").filter("#D1, #D3, #D4").hide();
       } else if ($(this).hasClass("C3")) {
         game.defender = document.getElementById("D3");
         game.defenderId = $("#D3").attr("id");
         $(".defender").filter("#D3").show();
-        $(".defender").filter("#D1, #D2, #D4, #D5").hide();
+        $(".defender").filter("#D1, #D2, #D4").hide();
       } else if ($(this).hasClass("C4")) {
         game.defender = document.getElementById("D4");
         game.defenderId = $("#D4").attr("id");
         $(".defender").filter("#D4").show();
-        $(".defender").filter("#D1, #D2, #D3, #D5").hide();
-      } else if ($(this).hasClass("C5")) {
-        game.defender = document.getElementById("D5");
-        game.defenderId = $("#D5").attr("id");
-        $(".defender").filter("#D5").show();
-        $(".defender").filter("#D1, #D2, #D3, #D4").hide();
+        $(".defender").filter("#D1, #D2, #D3").hide();
       }
       $("#ER").fadeTo(1000, 0.2);
       $("#enemies-row").fadeTo(1000, 0.2);
@@ -147,24 +140,21 @@ $(document).ready(function () {
     ///  $("#msg").text("Press button to attack the XXX");
     // $("#msg").text("");
     $("#gmsga, #gmsgd").text("");
-    $(" .emsg-row, .dmsg-row").hide();
+    $(" .emsg-row, .gmsg-row, .dmsg-row").hide();
     // $("#dmsg").hide();
     // $("#emsg, #gmsg1, #gmsg2").hide();
     $("span.C1").text($("#A1").attr("value"));
     $("span.C2").text($("#A2").attr("value"));
     $("span.C3").text($("#A3").attr("value"));
     $("span.C4").text($("#A4").attr("value"));
-    $("span.C5").text($("#A5").attr("value"));
     $("#A1").attr("health", ($("#A1").attr("value")));
     $("#A2").attr("health", ($("#A2").attr("value")));
-    $("#A3").attr("health", ($("#A3").attr("value")));
-    $("#A4").attr("health", ($("#A4").attr("value")));
-    $("#A5").attr("health", ($("#A5").attr("value")));
+    $("#A3").attr("health", ($("#A2").attr("value")));
+    $("#A4").attr("health", ($("#A2").attr("value")));
     $("#D1").attr("health", ($("#D1").attr("value")));
     $("#D2").attr("health", ($("#D2").attr("value")));
-    $("#D3").attr("health", ($("#D3").attr("value")));
-    $("#D4").attr("health", ($("#D4").attr("value")));
-    $("#D5").attr("health", ($("#D5").attr("value")));
+    $("#D3").attr("health", ($("#D2").attr("value")));
+    $("#D4").attr("health", ($("#D2").attr("value")));
     $(".attacker").attr("points", "0");
   };
 
@@ -172,7 +162,6 @@ $(document).ready(function () {
     $("#msg").text("Choose your attacker");
     $("#btn").hide();
     $("#AR").show();
-    $("#gmsga, #gmsgd").text("");
   };
 
   function attackGame() {
@@ -205,29 +194,25 @@ $(document).ready(function () {
     //   game.attacker.setAttribute("health", ahealth );
     //   game.defender.setAttribute("health", dhealth );
 
-    $("#gmsga").text("You attacked " + $(D).attr("char") + " with " + aattack + " damage points");
+    $("#gmsga").text("You attacked" + $(D).attr("char") + " with " + aattack + " damage points");
     $("#gmsgd").text($(D).attr("char") + " attacked you with " + dattack + " damage points");
 
     if (ahealth <= 0) {
+      $("#gmsga").text("Sorry you are out of health. You lost");
       $("#gmsgd").text("");
       game.losses++;
       updateStats();
       initGame();
-      $("#gmsga").text( $(A).attr("char") + " is out of health points. Sorry, You lost");
-      showDialog( "You are out of health points", "Sorry, You lost");
     } else if (dhealth <= 0) {
       game.killCnt++;
       if (game.killCnt === game.defenderCnt) {
         $("#gmsga").text("You Won this game");
         $("#gmsgd").text("");
-        $("#DR").hide();
-        $(" .emsg-row, .dmsg-row").hide();
         game.wins++;
         updateStats();
         $("#btn").text("Next Game");
         $("#btn").show();
         $("#msg").text("");
-        showDialog("Congratulations", "You WON");
       } else {
         nextDefender();
       }
@@ -258,20 +243,6 @@ $(document).ready(function () {
 
   };
 
-  function showDialog (headMsg, bodyMsg) {
-    $("#modal-header").text(headMsg);
-    $("#modal-body").text(bodyMsg);
-
-    $("#myModal").modal({
-      backdrop: "static",
-      keyboard: false // to prevent closing with Esc button (if you want this too)
-    });
-
-    this.dialogOpen = true;
-    // $("#myModal").on("shown.bs.modal", function() {
-    // $("#myInput").focus();
-    // });
-  }
 
   //$("#DR").document;
   // $("button").click(function(){
